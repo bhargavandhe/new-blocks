a9acaddc277ed634a7ffe2df412958f05290b50f
bhargavandhe <bhargavandhe2310.com>
2021-10-31 23:54:44 +0530

    v2

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { getDataFromFirebase } from "../helpers/database";
 
 function Dashboard() {
+
+
+  
+  useEffect(() => {
+    getDataFromFirebase()
+  }, [input])
+
   return <div>Dashboard</div>;
 }
 
